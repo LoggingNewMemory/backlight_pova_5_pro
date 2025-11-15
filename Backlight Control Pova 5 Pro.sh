e0f763eb57fd6c88ae0165c42b9feaa2896184f9
KanagawaYamada <yamadakernel.project@gmail.com>
2025-11-15 17:23:31 +0700

    Simplyfy the script
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- Backlight Control Pova 5 Pro.sh
+++ Backlight Control Pova 5 Pro.sh
@@ -3,7 +3,7 @@
 # ===     POVA 5 Pro    ===
 # === Backlight Control ===
 #
-# Version: Simplified Mod (Renamed)
+# Version: Command-line Argument Mod
 #
 
 # Enable LED function (set max current)
@@ -13,56 +13,38 @@ enable_led() {
     su -c 'echo 255 > /sys/class/leds/aw22xxx_led/brightness' 
 }
 
-# Main menu
-show_menu() {
-    clear
-    echo "=== POVA 5 Pro (Simplified) ==="
-    echo ""
-    echo "1. Flow"
-    echo "2. Breathing"
-    echo "3. Turn Off LEDs"
-    echo ""
-    echo "Q. Quit"
-    echo ""
-}
+# Read the first argument passed to the script
+choice="$1"
 
-# Main loop
-while true; do
-    show_menu
-    echo -ne "Select option [1-3/Q]: "
-    read main_choice
-    
-    case $main_choice in
-        1)
-            # Original: Menu 2, Option 2 (Music Party Mode)
-            enable_led
-            su -c 'echo 7 > /sys/class/leds/aw22xxx_led/effect'
-            su -c 'echo 13 > /sys/class/leds/aw22xxx_led/cfg'
-            echo "Flow activated"
-            sleep 1
-            ;;
-        2)
-            # Original: Menu 4, Option 1 (Skyline Incoming)
-            enable_led
-            su -c 'echo 4 > /sys/class/leds/aw22xxx_led/effect'
-            su -c 'echo 4 > /sys/class/leds/aw22xxx_led/cfg'
-            echo "Breathing activated"
-            sleep 1
-            ;;
-        3)
-            # Original: Menu 0
-            su -c 'echo 9 > /sys/class/leds/aw22xxx_led/cfg'
-            su -c 'echo 0 > /sys/class/leds/aw22xxx_led/hwen'
-            echo "LEDs turned off"
-            sleep 1
-            ;;
-        q|Q)
-            echo "Exiting..."
-            exit 0
-            ;;
-        *)
-            echo "Invalid choice!"
-            sleep 1
-            ;;
-    esac
-done
+case $choice in
+    1)
+        # Flow
+        enable_led
+        su -c 'echo 7 > /sys/class/leds/aw22xxx_led/effect'
+        su -c 'echo 13 > /sys/class/leds/aw22xxx_led/cfg'
+        echo "Flow activated"
+        ;;
+    2)
+        # Breathing
+        enable_led
+        su -c 'echo 4 > /sys/class/leds/aw22xxx_led/effect'
+        su -c 'echo 4 > /sys/class/leds/aw22xxx_led/cfg'
+        echo "Breathing activated"
+        ;;
+    3)
+        # Turn Off LEDs
+        su -c 'echo 9 > /sys/class/leds/aw22xxx_led/cfg'
+        su -c 'echo 0 > /sys/class/leds/aw22xxx_led/hwen'
+        echo "LEDs turned off"
+        ;;
+    *)
+        # Show usage if no argument or an invalid one is given
+        echo "Usage: $0 [1|2|3]"
+        echo "  1: Activate Flow"
+        echo "  2: Activate Breathing"
+        echo "  3: Turn Off LEDs"
+        exit 1
+        ;;
+esac
+
+exit 0
