4e3b51f91220652682b0a538456841cbd9f826ec
KanagawaYamada <yamadakernel.project@gmail.com>
2025-11-16 15:19:12 +0700

    64 Bit only
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- AppSource/pova_5_pro_backlight/android/app/build.gradle.kts
+++ AppSource/pova_5_pro_backlight/android/app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            ndk {
+                abiFilters.clear()
+                abiFilters.add("arm64-v8a")
+            }
         }
     }
 }
