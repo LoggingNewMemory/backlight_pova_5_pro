9fda041ac92646a20310c64dbc4ec60f621dba67
KanagawaYamada <yamadakernel.project@gmail.com>
2025-11-16 15:06:35 +0700

    A bit animation to the button
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- AppSource/pova_5_pro_backlight/lib/main.dart
+++ AppSource/pova_5_pro_backlight/lib/main.dart
@@ -128,10 +128,8 @@ class _HomePageState extends State<HomePage> {
   Future<void> _launchDonateLink() async {
     final Uri url = Uri.parse('https://t.me/KanagawaYamadaCH/2543');
     try {
-      // Launches the URL in an external browser/app
       await launchUrl(url, mode: LaunchMode.externalApplication);
     } catch (e) {
-      // You could show an error message to the user here
       debugPrint('Could not launch $url: $e');
     }
   }
@@ -274,7 +272,7 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class AppButton extends StatelessWidget {
+class AppButton extends StatefulWidget {
   final String label;
   final VoidCallback onPressed;
   final Gradient gradient;
@@ -287,18 +285,86 @@ class AppButton extends StatelessWidget {
   });
 
   @override
+  State<AppButton> createState() => _AppButtonState();
+}
+
+class _AppButtonState extends State<AppButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  Animation<double>? _slashPosition;
+  Animation<double>? _slashOpacity;
+  Animation<double>? _breathOpacity;
+  Animation<double>? _pixelOpacity;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.label == 'FLOW') {
+      _controller = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 400),
+      );
+      _slashPosition = Tween<double>(
+        begin: -0.5,
+        end: 1.5,
+      ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+      _slashOpacity = TweenSequence<double>([
+        TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.7), weight: 40),
+        TweenSequenceItem(tween: Tween(begin: 0.7, end: 0.0), weight: 60),
+      ]).animate(_controller);
+    } else if (widget.label == 'BREATHING') {
+      _controller = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 1500),
+      );
+      _breathOpacity = TweenSequence<double>([
+        TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.4), weight: 50),
+        TweenSequenceItem(tween: Tween(begin: 0.4, end: 0.0), weight: 50),
+      ]).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+    } else if (widget.label == 'TURN OFF LED') {
+      _controller = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 400),
+      );
+      _pixelOpacity = TweenSequence<double>([
+        TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.2), weight: 1),
+        TweenSequenceItem(tween: Tween(begin: 0.2, end: 1.0), weight: 1),
+        TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.5), weight: 1),
+        TweenSequenceItem(tween: Tween(begin: 0.5, end: 1.0), weight: 1),
+        TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.0), weight: 1),
+        TweenSequenceItem(tween: Tween(begin: 0.0, end: 1.0), weight: 1),
+      ]).animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+    } else {
+      _controller = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 100),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 60,
       decoration: BoxDecoration(
-        gradient: gradient,
+        gradient: widget.gradient,
         borderRadius: BorderRadius.circular(12),
       ),
       child: Material(
         color: Colors.transparent,
         child: InkWell(
-          onTap: onPressed,
+          onTap: () {
+            _controller.forward(from: 0.0);
+            widget.onPressed();
+          },
           borderRadius: BorderRadius.circular(12),
           child: Container(
             margin: const EdgeInsets.all(2.5),
@@ -306,15 +372,64 @@ class AppButton extends StatelessWidget {
               color: Colors.black,
               borderRadius: BorderRadius.circular(9.5),
             ),
-            child: Center(
-              child: Text(
-                label,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w300,
-                  letterSpacing: 1.2,
-                ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(9.5),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return AnimatedBuilder(
+                    animation: _controller,
+                    child: Center(
+                      child: Text(
+                        widget.label,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w300,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ),
+                    builder: (context, child) {
+                      Widget content = child!;
+
+                      if (widget.label == 'TURN OFF LED' &&
+                          _pixelOpacity != null) {
+                        content = Opacity(
+                          opacity: _pixelOpacity!.value,
+                          child: child,
+                        );
+                      }
+
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          content,
+                          if (widget.label == 'FLOW' && _slashPosition != null)
+                            Positioned(
+                              left:
+                                  constraints.maxWidth * _slashPosition!.value,
+                              top: 0,
+                              bottom: 0,
+                              width: constraints.maxWidth * 0.4,
+                              child: Container(
+                                transform: Matrix4.skewX(-0.3),
+                                color: Colors.white.withOpacity(
+                                  _slashOpacity!.value,
+                                ),
+                              ),
+                            ),
+                          if (widget.label == 'BREATHING' &&
+                              _breathOpacity != null)
+                            Container(
+                              color: Colors.white.withOpacity(
+                                _breathOpacity!.value,
+                              ),
+                            ),
+                        ],
+                      );
+                    },
+                  );
+                },
               ),
             ),
           ),
